f6b1be0eff70e3842a660c970d8746016b559419
Aykut Sarac <61199137+aykut-sarac@users.noreply.github.com>
2020-05-18 16:11:36 +1000

    Add new project

---
+++ build/precache-manifest.dc346e6687e9f8b38b54efdb976d7046.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb32b601e96e668a7a388910b46e5b25",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "f4a18834b0e4d55e8f15",
+    "url": "/myapp/static/css/main.6aacfd87.chunk.css"
+  },
+  {
+    "revision": "a894c03e22684998f4ef",
+    "url": "/myapp/static/js/2.2d087320.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/myapp/static/js/2.2d087320.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4a18834b0e4d55e8f15",
+    "url": "/myapp/static/js/main.f76ec387.chunk.js"
+  },
+  {
+    "revision": "7c157e24c57b3c3aa05a",
+    "url": "/myapp/static/js/runtime-main.6d446693.js"
+  },
+  {
+    "revision": "aee312c4f56918f4f48e9a8551a75741",
+    "url": "/myapp/static/media/Resume.aee312c4.pdf"
+  },
+  {
+    "revision": "f6082ca6324bb65be055be3d6589148d",
+    "url": "/myapp/static/media/about.f6082ca6.jpg"
+  },
+  {
+    "revision": "d53fed8c61ca8d40df2409f1088ef572",
+    "url": "/myapp/static/media/banner.d53fed8c.jpg"
+  },
+  {
+    "revision": "12264855aff6d40187583190f9138afb",
+    "url": "/myapp/static/media/batusgif.12264855.gif"
+  },
+  {
+    "revision": "7af0911c7c1ea328696e606eac170ba3",
+    "url": "/myapp/static/media/bloggif.7af0911c.gif"
+  },
+  {
+    "revision": "49f6a4761651391ef0ccf2588ce409e3",
+    "url": "/myapp/static/media/ecomgif.49f6a476.gif"
+  },
+  {
+    "revision": "9e42746d27e7ef1792bdee36cafacacc",
+    "url": "/myapp/static/media/ozgif.9e42746d.gif"
+  },
+  {
+    "revision": "4865394bf48a7a5021d7eea2212a733f",
+    "url": "/myapp/static/media/tweetmegif.4865394b.gif"
+  }
+]);
